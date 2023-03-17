75dced5f51b75b6da264dbba62c76364def99dcc
carlos-hr <carlos.henrique17.chr@gmail.com>
2023-03-17 09:45:49 -0300

    feat: setup styled components

--- App.tsx
+++ App.tsx
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  useFonts,
+  NunitoSans_400Regular,
+  NunitoSans_700Bold,
+} from "@expo-google-fonts/nunito-sans";
+import { StatusBar, View } from "react-native";
+import styled, { ThemeProvider } from "styled-components/native";
+import { theme } from "./src/theme";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ NunitoSans_400Regular, NunitoSans_700Bold });
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <View style={{ justifyContent: "center", alignItems: "center", flex: 1 }}>
+        <Title>Initial Setup</Title>
+      </View>
+    </ThemeProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Title = styled.Text`
+  font-family: ${({ theme }) => theme.font_family.bold};
+`;
