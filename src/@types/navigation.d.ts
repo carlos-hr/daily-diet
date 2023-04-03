3c0075930f60e6e0b1b5cda66eb40980ef995556
carlos-hr <carlos.henrique17.chr@gmail.com>
2023-04-03 19:15:26 -0300

    fix: adjusting route params

---
+++ src/@types/navigation.d.ts
@@ -0,0 +1,12 @@
+export declare global {
+  namespace ReactNavigation {
+    interface RootParamList {
+      home: undefined;
+      statistics: undefined;
+      "new-meal": undefined;
+      feedback: {
+        type: "positive" | "negative";
+      };
+    }
+  }
+}
