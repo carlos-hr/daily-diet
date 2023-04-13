a8bd9188bfef2783c6b92c3d1d7b1534fa69c766
carlos-hr <carlos.henrique17.chr@gmail.com>
2023-04-13 20:51:17 -0300

    feat: start styling new meal page

--- src/screens/NewMeal/index.tsx
+++ src/screens/NewMeal/index.tsx
@@ -1,18 +1,18 @@
 import { Header } from "@components/Header";
 import { HighlightCard } from "@components/HighlightCard";
 import { Text } from "react-native";
-import { Container, HeaderTextContainer } from "./styles";
+import { Container, HeaderText, NewMealFormContainer } from "./styles";
 
 export function NewMeal() {
   return (
     <Container>
       <Header type="tertiary" showBackButton>
-        <HeaderTextContainer>
-          <HighlightCard type="neutral">
-            <Text>Nova refeição</Text>
-          </HighlightCard>
-        </HeaderTextContainer>
+        <HighlightCard type="neutral">
+          <HeaderText>Nova refeição</HeaderText>
+        </HighlightCard>
       </Header>
+
+      <NewMealFormContainer></NewMealFormContainer>
     </Container>
   );
 }
