f0217a0b3e2d94a9b7e52f8bdc090c1722f1fced
carlos-hr <carlos.henrique17.chr@gmail.com>
2023-04-13 20:39:52 -0300

    fix: adjusting styled-components bug

--- src/screens/NewMeal/styles.ts
+++ src/screens/NewMeal/styles.ts
@@ -1,13 +1,5 @@
 import styled from "styled-components/native";
-import { Text } from "react-native";
-import { TextHighlight } from "@components/HighlightCard/styles";
 
 export const Container = styled.View``;
 
-export const HeaderTextContainer = styled.View`
-  & > ${TextHighlight} {
-    font-size: 20px;
-    font-weight: bold;
-    color: #000;
-  }
-`;
+export const HeaderTextContainer = styled.View``;
